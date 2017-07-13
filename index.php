42272c2d799b609f49cc93d1e82f4053b8f839c9
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2017-07-13 18:29:19 +0200

    Project init

---
+++ index.php
@@ -0,0 +1,40 @@
+<html lang="en">
+	<head>
+		<title>Gamerecord • Record and manage your game stats</title>
+		
+        <link rel="stylesheet" href="./style/main.css">
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,800,900" rel="stylesheet">
+		<!-- <link rel="icon" href="./img/favicon.png"> -->
+        <!-- <link rel="apple-touch-icon" sizes="120x120" href="../ressources/touch-icon-iphone.png"> -->
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+		
+	</head>
+	<body>
+
+		<div id="fixed-header">
+
+			<div id="user-header">
+				<span class="first">Matthieu Harbonnier</span>
+				<img src="./ressources/profile_pic.png" class="profile_pic">
+			</div>
+
+			<a href="#" class="submit_btn new_game_btn">New Game</a>
+
+		</div>
+
+		<div id="above-header-content">
+			<div id="fixed-left-menu">
+				left menu
+			</div>
+
+			<div id="content">
+				Hello world! <br> Some content here
+			</div>
+		</div>
+
+
+
+	</body>
+</html>
